28715b9bdc3f7b3ab9f08a3260dd0378aa780c0b
Roy SALIBA <134060194+Royalcoco@users.noreply.github.com>
2023-08-09 02:36:31 +0200

    Add files via upload

---
+++ HTML_V2.jsx
@@ -0,0 +1,217 @@
+// Script JavaScript pour gérer les interactions avec la page
+document.addEventListener("DOMContentLoaded", function () {
+    const changeFont = document.getElementById("changeFont");
+    const changeSize = document.getElementById("changeSize");
+    const alignText = document.getElementById("alignText");
+
+    changeFont.addEventListener("click", function () {
+        changeFont.style.fontFamily = "Courier New, monospace";
+    });
+
+    changeSize.addEventListener("click", function () {
+        changeSize.style.fontSize = "24px";
+    });
+
+    alignText.addEventListener("click", function () {
+        alignText.style.textAlign = "center";
+    });
+});
+
+// Path: HTML_V3.jsx
+// Script JavaScript pour gérer les interactions avec la page
+document.addEventListener("DOMContentLoaded", function () {
+    const changeFont = document.getElementById("changeFont");
+    const changeSize = document.getElementById("changeSize");
+    const alignText = document.getElementById("alignText");
+
+    changeFont.addEventListener("click", function () {
+        changeFont.style.fontFamily = "Courier New, monospace";
+    });
+
+    changeSize.addEventListener("click", function () {
+        changeSize.style.fontSize = "24px";
+    });
+
+    alignText.addEventListener("click", function () {
+        alignText.style.textAlign = "center";
+    });
+}
+);
+
+// Path: HTML_V4.jsx
+// Script JavaScript pour gérer les interactions avec la page
+document.addEventListener("DOMContentLoaded", function () {
+    const changeFont = document.getElementById("changeFont");
+    const changeSize = document.getElementById("changeSize");
+    const alignText = document.getElementById("alignText");
+
+    changeFont.addEventListener("click", function () {
+        changeFont.style.fontFamily = "Courier New, monospace";
+    });
+
+    changeSize.addEventListener("click", function () {
+        changeSize.style.fontSize = "24px";
+    });
+
+    alignText.addEventListener("click", function () {
+        alignText.style.textAlign = "center";
+    });
+}
+);
+
+// Path: HTML_V5.jsx
+// Script JavaScript pour gérer les interactions avec la page
+document.addEventListener("DOMContentLoaded", function () {
+    const changeFont = document.getElementById("changeFont");
+    const changeSize = document.getElementById("changeSize");
+    const alignText = document.getElementById("alignText");
+
+    changeFont.addEventListener("click", function () {
+        changeFont.style.fontFamily = "Courier New, monospace";
+    });
+
+    changeSize.addEventListener("click", function () {
+        changeSize.style.fontSize = "24px";
+    });
+
+    alignText.addEventListener("click", function () {
+        alignText.style.textAlign = "center";
+    });
+}
+);
+
+// Path: HTML_V6.jsx
+// Script JavaScript pour gérer les interactions avec la page
+document.addEventListener("DOMContentLoaded", function () {
+    const changeFont = document.getElementById("changeFont");
+    const changeSize = document.getElementById("changeSize");
+    const alignText = document.getElementById("alignText");
+
+    changeFont.addEventListener("click", function () {
+        changeFont.style.fontFamily = "Courier New, monospace";
+    });
+
+    changeSize.addEventListener("click", function () {
+        changeSize.style.fontSize = "24px";
+    });
+
+    alignText.addEventListener("click", function () {
+        alignText.style.textAlign = "center";
+    });
+}
+);
+
+// Path: HTML_V7.jsx
+// Script JavaScript pour gérer les interactions avec la page
+document.addEventListener("DOMContentLoaded", function () {
+    const changeFont = document.getElementById("changeFont");
+    const changeSize = document.getElementById("changeSize");
+    const alignText = document.getElementById("alignText");
+
+    changeFont.addEventListener("click", function () {
+        changeFont.style.fontFamily = "Courier New, monospace";
+    });
+
+    changeSize.addEventListener("click", function () {
+        changeSize.style.fontSize = "24px";
+    });
+
+    alignText.addEventListener("click", function () {
+        alignText.style.textAlign = "center";
+    });
+}
+);
+
+// Path: HTML_V8.jsx
+// Script JavaScript pour gérer les interactions avec la page
+document.addEventListener("DOMContentLoaded", function () {
+    const changeFont = document.getElementById("changeFont");
+    const changeSize = document.getElementById("changeSize");
+    const alignText = document.getElementById("alignText");
+
+    changeFont.addEventListener("click", function () {
+        changeFont.style.fontFamily = "Courier New, monospace";
+    });
+
+    changeSize.addEventListener("click", function () {
+        changeSize.style.fontSize = "24px";
+    });
+
+    alignText.addEventListener("click", function () {
+        alignText.style.textAlign = "center";
+    });
+}
+);
+
+// Path: HTML_V9.jsx
+// Script JavaScript pour gérer les interactions avec la page
+document.addEventListener("DOMContentLoaded", function () {
+    const changeFont = document.getElementById("changeFont");
+    const changeSize = document.getElementById("changeSize");
+    const alignText = document.getElementById("alignText");
+
+    changeFont.addEventListener("click", function () {
+        changeFont.style.fontFamily = "Courier New, monospace";
+    });
+
+    changeSize.addEventListener("click", function () {
+        changeSize.style.fontSize = "24px";
+    });
+
+    alignText.addEventListener("click", function () {
+        alignText.style.textAlign = "center";
+    });
+}
+);
+
+// Path: HTML_V10.jsx
+// Script JavaScript pour gérer les interactions avec la page
+document.addEventListener("DOMContentLoaded", function () {
+    const changeFont = document.getElementById("changeFont");
+    const changeSize = document.getElementById("changeSize");
+    const alignText = document.getElementById("alignText");
+    const changeColor = document.getElementById("changeColor");
+
+    changeFont.addEventListener("click", function () {
+        changeFont.style.fontFamily = "Courier New, monospace";
+    });
+
+    changeSize.addEventListener("click", function () {
+        changeSize.style.fontSize = "24px";
+    });
+
+    changeColor.addEventListener("click", function () {
+        changeColor.style.color = "red";
+    });
+
+    alignText.addEventListener("click", function () {
+        alignText.style.textAlign = "center";
+    });
+}
+);
+
+// Path: HTML_V11.jsx
+// Script JavaScript pour gérer les interactions avec la page
+document.addEventListener("DOMContentLoaded", function () {
+    const changeFont = document.querySelector("#changeFont");
+    const changeSize = document.querySelector("#changeSize");
+    const alignText = document.querySelector("#alignText");
+    const changeColor = document.querySelector("#changeColor");
+
+    changeFont.addEventListener("click", function () {
+        changeFont.style.fontFamily = "Courier New, monospace";
+    });
+
+    changeSize.addEventListener("click", function () {
+        changeSize.style.fontSize = "24px";
+    });
+
+    changeColor.addEventListener("click", function () {
+        changeColor.style.color = "red";
+    });
+
+    alignText.addEventListener("click", function () {
+        alignText.style.textAlign = "center";
+    });
+}
+);
