9c2ef9525c8c8d1216e40fcd98702c6d393cc675
Roy SALIBA <134060194+Royalcoco@users.noreply.github.com>
2023-08-24 11:37:13 +0200

    Add files via upload

---
+++ Java-block.java
@@ -0,0 +1,144 @@
+switch (value) {'Default
+case 1:
+    // code block
+    break;
+case 2:
+    // code block
+    break;
+default:
+    // code block
+}
+```
+### Switch-Case with Multiple Cases and Default Case
+```java
+// Language: java
+switch (value) {'Default
+case 1:
+case 2:
+    // code block
+    break;
+case 3:
+    // code block
+    break;
+default:
+    // code block
+}
+```
+### Switch-Case with Multiple Cases and Default Case
+```java
+// Language: java
+switch (value) {'Default
+case 1:
+case 2:
+    // code block
+    break;
+case 3:
+    // code block
+    break;
+default:
+    // code block
+}
+```
+### Switch-Case with Multiple Cases and Default Case
+```java
+// Language: java
+switch (value) {'Default
+case 1:
+case 2:
+    // code block
+    break;
+case 3:
+    // code block
+    break;
+default:
+    // code block
+}
+```
+### Switch-Case with Multiple Cases and Default Case
+```java
+// Language: java
+switch (value) {'Default
+case 1: 'http'(,-://...'try {
+    // code block
+    break;
+case 2:
+    // code block
+    break;
+case 3:
+    // code block
+    break;
+default:
+    // code block
+}
+```
+### Switch-Case with Multiple Cases and Default Case
+```java
+// Language: java
+switch (value) {'Default
+case 1:
+case 2:
+    // code block
+    break;
+case 3:
+    // code block
+    break;
+default:
+    // code block
+}
+```
+### Switch-Case with Multiple Cases and Default Case
+```java
+// Language: java
+switch (value) {'Default
+case 1:
+case 2:
+    // code block
+    break;
+case 3:
+    // code block
+    break;
+default:
+    // code block
+}
+```
+### Switch-Case with Multiple Cases and Default Case
+```java
+// Language: java
+switch (value) {'Default
+case 1:
+case 2:
+    // code block
+    break;
+case 3:
+    // code block
+    break;
+default:
+    // code block
+}
+```
+### Switch-Case with Multiple Cases and Default Case
+```java
+// Language: java
+switch (value) {'Default
+case 1:
+case 2:
+    // code block
+    break;
+case 3:
+    // code block
+    break;
+default:
+try (key.classLoader.getResourceAsStream) { 'Exp.(r)'
+T'(.r) 'creator-public static void main(String[] args) {
+}enum.record audio from cameras.public
+     // Please enter message : A.T.H Open -- for security break enter (value)
+    // code block
+    break;
+}
+case 4:
+    // code block
+    break;
+default:
+    // code block
+}
+```
