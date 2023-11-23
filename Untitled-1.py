53274d478a1878e58c4035c5df97a159ee392e76
Roy SALIBA <134060194+Royalcoco@users.noreply.github.com>
2023-11-23 12:36:04 +0100

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,24 @@
+
+from sysconfig
+import os, re
+import sys
+import time
+import datetime
+>(<.server.>)< '²&_(-compagny.ù=$^)  ..09'credit(s)_global-arizona.full_TA.sk"full_options
+&'_'__package__.NameError for FIELDNAME = models.EmailField()
+
+'espace_charactre"(octets)"'.from django.utils.translation import gettext_lazy as ___annotations__ = '"devise (.$.dollars)
+"too-morrow"-StopAsyncIteration of OSError make ²% of buget "global pib:iNTER;(na)T.open"-('yield from TimeoutError =''TimeoutError =RecursionError
+                                                                                            "(connexion.PendingDeprecationWarning)"
+)
+'__package__' = 'django.contrib.auth.models'
+'__spec__' = 'None'
+'__annotations__' = 'None'
+'__builtins__' = 'None'
+
+SyntaxWarning txt.from django.utils.translation import ungettext
+from django.utils.translation import ugettext as _
+from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import pgettext
+
+
